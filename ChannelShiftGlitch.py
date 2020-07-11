98ecf2bc1903ea8b51e293d11ce58aa3e144d30b
ranayash45 <y.rana18@outlook.com>
2020-07-11 12:21:58 +0530

    GlitchImageAdded

--- ChannelShiftGlitch.py
+++ ChannelShiftGlitch.py
@@ -2,7 +2,9 @@ import cv2
 import time
 import random
 img = cv2.imread(r'demo.jpg')
-img = cv2.resize(img,(512,512))
+width = 500
+height = round(width / img.shape[0] * img.shape[1])
+img = cv2.resize(img,(height,width))
 frame = img.copy()
 n = 5
 m = 5
@@ -19,4 +21,5 @@ while True:
     frame[:frame.shape[0] - m,:frame.shape[1] - n,1] = img[m:,n:,1] 
     
     cv2.imshow('demo',frame)
+    cv2.imwrite('ChannelShiftGlitch.jpg',frame)
     cv2.waitKey(100)
